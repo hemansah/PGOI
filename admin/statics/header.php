44006fb9523bc44b31ccdc05146629eb1ebd5d19
unknown <bhadouriya.09@gmail.com>
2019-04-12 03:07:05 +0530

    ui

--- admin/statics/header.php
+++ admin/statics/header.php
@@ -20,24 +20,24 @@
                 </li>
 
                 <li>
-                    <a href="#"><i class="fa fa-clipboard"></i> Attendance</a>
+                    <a href="events_admin.php"><i class="fa fa-calendar"></i> Events</a>
                 </li>
+<!-- 
+                <li>
+                    <a href="#"><i class="fa fa-clipboard"></i> Attendance</a>
+                </li> -->
 
                 <li>
                     <a href="bus_route_admin.php"><i class="fa fa-bus"></i> Bus Route</a>
                 </li>
-
-                <li>
-                    <a href="events_admin.php"><i class="fa fa-calendar"></i> Events</a>
-                </li>
-
+<!-- 
                 <li>
                     <a href="#"><i class="fa fa-building"></i> Training & Placement</a>
                 </li>
 
                 <li>
                     <a href="#"><i class="fa fa-book"></i> Syllabus</a>
-                </li>
+                </li> -->
 
                 <li>
                     <a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a>
--- statics/footer.php
+++ statics/footer.php
@@ -1,14 +1,13 @@
 		<footer class="footer1">
 			<div class="row">
-				<div class="col-10">
-					<p style="text-align: center;">&copy; Patel Group of Institutions 2019</p>
-				</div>
+				<div class="col-12">
+					<p style="display: inline;">&copy; Patel Group of Institutions 2019</p>
+					<div class="pull-right">
+						<a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-2x social"></i></a>
+						<a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-twitter-square fa-2x social"></i></a>
+						<a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-envelope-square fa-2x social"></i></a>
 
-				<div class="col-2">
-					<a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-2x social"></i></a>
-					<a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-twitter-square fa-2x social"></i></a>
-					<a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-envelope-square fa-2x social"></i></a>
-
+					</div>
 				</div>
 			</div>
 
